974bb4aaba21f8ac7fb6957d742a12ce2ef123e3
Marvin M <39344769+M123-dev@users.noreply.github.com>
2025-02-04 22:27:58 +0100

    Entrypoint for DB?

--- maybe_finance/run.sh
+++ maybe_finance/run.sh
@@ -41,5 +41,9 @@ export HOSTING_PLATFORM="localhost"
 [ -n "$PLAID_SECRET" ] && export PLAID_SECRET="${PLAID_SECRET}"
 [ -n "$PLAID_ENV" ] && export PLAID_ENV="${PLAID_ENV}"
 
+# Call the original docker-entrypoint script
+echo "Running original docker-entrypoint script..."
+/rails/bin/docker-entrypoint
+
 # Start the Rails server
 exec ./bin/rails server
