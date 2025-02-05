035e0e59fdaa5e863fa3447a14e4245657b81f50
Marvin M <39344769+M123-dev@users.noreply.github.com>
2025-02-05 11:14:00 +0100

    Ingress + domain

--- maybe_finance/run.sh
+++ maybe_finance/run.sh
@@ -13,7 +13,7 @@ SECRET_KEY_BASE=$(jq --raw-output '.secret_key_base' "$CONFIG_PATH")
 DB_HOST=$(jq --raw-output '.db_host // "172.30.32.1"' "$CONFIG_PATH")
 DB_PORT=$(jq --raw-output '.db_port // "5432"' "$CONFIG_PATH")
 POSTGRES_USER=$(jq --raw-output '.postgres_user // "postgres"' "$CONFIG_PATH")
-APP_DOMAIN=$(jq --raw-output '.app_domain // "http://localhost"' "$CONFIG_PATH")
+#APP_DOMAIN=$(jq --raw-output '.app_domain // "http://localhost"' "$CONFIG_PATH")
 PLAID_CLIENT_ID=$(jq --raw-output '.plaid_client_id // empty' "$CONFIG_PATH")
 PLAID_SECRET=$(jq --raw-output '.plaid_secret // empty' "$CONFIG_PATH")
 PLAID_ENV=$(jq --raw-output '.plaid_env // empty' "$CONFIG_PATH")
@@ -31,10 +31,10 @@ export SECRET_KEY_BASE="${SECRET_KEY_BASE}"
 export DB_HOST="${DB_HOST}"
 export DB_PORT="${DB_PORT}"
 export POSTGRES_USER="${POSTGRES_USER}"
-export APP_DOMAIN="${APP_DOMAIN}"
-export SELF_HOSTED="true"
-export DISABLE_SSL="false"
-export HOSTING_PLATFORM="localhost"
+#export APP_DOMAIN="${APP_DOMAIN}"
+#export SELF_HOSTED="true"
+#export DISABLE_SSL="false"
+#export HOSTING_PLATFORM="localhost"
 
 # Export optional Plaid configuration if set
 [ -n "$PLAID_CLIENT_ID" ] && export PLAID_CLIENT_ID="${PLAID_CLIENT_ID}"
